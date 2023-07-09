85284609cea32a32cf6b45efa3cacf819d948dc9
Andrei Chirkov <aachirkov@me.com>
2023-07-09 18:09:28 +0300

    Start project with custom webpack config

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Counter from "./components/Counter"
+import "./index.scss"
+
+const App = () => {
+  return (
+    <div className="app">
+      Бла
+      <Counter />
+    </div>
+  )
+}
+
+export default App
