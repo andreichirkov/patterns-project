e19194072c2738c2c9d0bb44a6ca6d0814e05da4
Andrei Chirkov <aachirkov@me.com>
2023-07-10 11:58:59 +0300

    Add scss structure & 2 themes with context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import React, { Suspense } from "react"
+import "./styles/index.scss"
+import { Suspense, useContext } from "react"
 import Counter from "./components/Counter"
-import "./index.scss"
 import { Link, Route, Routes } from "react-router-dom"
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async"
 import { MainPageAsync } from "./pages/MainPage/MainPage.async"
+import { useTheme } from "./theme/useTheme"
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme()
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle theme</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>About</Link>
       <Suspense fallback={<div>Loading</div>}>
---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,23 @@
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext"
+import { FC, useMemo, useState } from "react"
+
+// Каст к типу потому что LC вернет строку, а тут используется Enum
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT
+
+const ThemeProvider: FC = ({children}) => {
+  const [theme, setTheme] = useState<Theme>(defaultTheme)
+
+
+  const defaultProps = useMemo(() => ({
+    theme: theme,
+    setTheme: setTheme
+  }), [theme])
+
+  return (
+    <ThemeContext.Provider value={defaultProps}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export default ThemeProvider
