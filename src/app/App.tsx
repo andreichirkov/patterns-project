f7d879a80fae27a1f5c66d36d242f8252aea09a9
Andrei Chirkov <aachirkov@me.com>
2023-07-11 18:18:17 +0300

    Add router config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,18 @@
 import "./styles/index.scss"
-import { Suspense } from "react"
-import { Link, Route, Routes } from "react-router-dom"
+import { Link } from "react-router-dom"
 import { classNames } from "shared/lib/classNames/classNames"
 import { useTheme } from "app/providers/ThemeProvider"
-import { AboutPage } from "pages/AboutPage"
-import { MainPage } from "pages/MainPage"
+import { AppRouter } from "app/providers/router"
 
 const App = () => {
   const { theme, toggleTheme } = useTheme()
 
   return (
-    <div className={classNames('app', {}, [theme])}>
+    <div className={classNames("app", {}, [theme])}>
       <button onClick={toggleTheme}>Toggle theme</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>About</Link>
-      <Suspense fallback={<div>Loading</div>}>
-        <Routes>
-          <Route path={"/"} element={<MainPage />} />
-          <Route path={"/about"} element={<AboutPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   )
 }
---
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -0,0 +1,25 @@
+import { RouteProps } from "react-router-dom"
+import { MainPage } from "pages/MainPage"
+import { AboutPage } from "pages/AboutPage"
+
+export enum AppRoutes {
+  MAIN = "main",
+  ABOUT = "about"
+}
+
+/* Такая конструкция, чтобы явно соблюдать Enum */
+export const RoutePath: Record<AppRoutes, string> = {
+  [AppRoutes.MAIN]: "/",
+  [AppRoutes.ABOUT]: "/about"
+}
+
+export const routeConfig: Record<AppRoutes, RouteProps> = {
+  [AppRoutes.MAIN]: {
+    path: RoutePath.main,
+    element: <MainPage />
+  },
+  [AppRoutes.ABOUT]: {
+    path: RoutePath.about,
+    element: <AboutPage />
+  }
+}
