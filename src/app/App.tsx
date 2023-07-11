8fc744fa0d9a986396938ed0001555dd8e531cc9
Andrei Chirkov <aachirkov@me.com>
2023-07-11 12:27:25 +0300

    Change pages by FSD (Feature-Sliced Design) too

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import "./styles/index.scss"
 import { Suspense } from "react"
 import { Link, Route, Routes } from "react-router-dom"
-import { AboutPageAsync } from "pages/AboutPage/AboutPage.async"
-import { MainPageAsync } from "pages/MainPage/MainPage.async"
 import { classNames } from "hepers/classNames/classNames"
 import { useTheme } from "app/providers/ThemeProvider"
+import { AboutPage } from "pages/AboutPage"
+import { MainPage } from "pages/MainPage"
 
 const App = () => {
   const { theme, toggleTheme } = useTheme()
@@ -16,8 +16,8 @@ const App = () => {
       <Link to={"/about"}>About</Link>
       <Suspense fallback={<div>Loading</div>}>
         <Routes>
-          <Route path={"/"} element={<MainPageAsync />} />
-          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPage />} />
+          <Route path={"/about"} element={<AboutPage />} />
         </Routes>
       </Suspense>
     </div>
