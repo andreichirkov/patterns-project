8fc45d03d5a4bb96a120bcf5a2b98ac5d774ba3b
Andrei Chirkov <aachirkov@me.com>
2023-07-12 11:39:23 +0300

    Add SVGR and file loader to webpack, create base Button

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,7 +1,16 @@
-declare module '*.scss' {
-    interface IClassNames {
-        [className: string]: string
-    }
-    const classnames: IClassNames;
-    export = classnames;
+declare module "*.scss" {
+  interface IClassNames {
+    [className: string]: string
+  }
+
+  const classnames: IClassNames
+  export = classnames
+}
+
+declare module "*.png"
+declare module "*.jpg"
+declare module "*.jpeg"
+declare module "*.svg" {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>
+  export default content
 }
