a2b5b31ef8ad0de7f76edc37317970a9afd77e55
Andrei Chirkov <aachirkov@me.com>
2023-07-23 12:43:45 +0300

    Add chunked i18n plugin & LangSwitcher component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,5 @@
 import "./styles/index.scss"
+import { Suspense } from "react"
 import { classNames } from "shared/lib/classNames/classNames"
 import { useTheme } from "app/providers/ThemeProvider"
 import { AppRouter } from "app/providers/router"
@@ -10,11 +11,13 @@ const App = () => {
 
   return (
     <div className={classNames("app", {}, [theme])}>
-      <Navbar />
-      <div className='content-page'>
-        <Sidebar />
-        <AppRouter />
-      </div>
+      <Suspense fallback=''>
+        <Navbar />
+        <div className='content-page'>
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   )
 }
