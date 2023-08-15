8ccd80ec5fbf51c86d4ae3cf265043c29e74a589
Andrei Chirkov <aachirkov@me.com>
2023-08-15 18:39:04 +0300

    Add Sidebar test toggle button

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,11 +1,19 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { withTranslation } from "react-i18next"
 import { renderWIthTranslation } from "shared/lib/tests/renderWIthTranslation/renderWIthTranslation"
 import { Sidebar } from "./Sidebar"
 
 describe("Sidebar", () => {
-  test("Есть ли в ХТМЛ", () => {
+  test("is in HTML", () => {
     renderWIthTranslation(<Sidebar />)
     expect(screen.getByTestId("sidebar")).toBeInTheDocument()
+  })
+
+  test("toggle button", () => {
+    renderWIthTranslation(<Sidebar />)
+    const toggleButton = screen.getByTestId("sidebar-toggle")
+    expect(screen.getByTestId("sidebar")).toBeInTheDocument()
+    fireEvent.click(toggleButton)
+    expect(screen.getByTestId("sidebar")).toHaveClass("collapsed")
   })
 })
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -13,7 +13,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
   const [collapsed, setCollapsed] = useState(false)
 
   const onToggle = () => {
-    setCollapsed((prev) => !prev)
+    setCollapsed(prev => !prev)
   }
 
   return (
@@ -22,7 +22,9 @@ export const Sidebar = ({ className }: SidebarProps) => {
       className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
         className
       ])}>
-      <Button onClick={onToggle}>toggle</Button>
+      <Button data-testid="sidebar-toggle" onClick={onToggle}>
+        toggle
+      </Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwitcher className={cls.lang} />
