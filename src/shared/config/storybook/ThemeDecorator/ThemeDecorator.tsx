67def3c84f7e8e74e310e74c87ca41c0f01b249e
Andrei Chirkov <aachirkov@me.com>
2023-08-23 21:13:07 +0300

    Try to fix storybook fix errors

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -3,6 +3,6 @@ import { Theme } from "app/providers/ThemeProvider";
 
 export const ThemeDecorator = (theme: Theme) => (StoryComponent: () => Story) => (
   <div className={`app ${theme}`}>
-    <StoryComponent />
+    {StoryComponent()}
   </div>
 )
