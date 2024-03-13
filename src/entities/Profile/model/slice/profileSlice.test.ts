b0d1b7992ad3b490afadf5a90034226a0e6fd794
Andrei Chirkov <aachirkov@me.com>
2024-03-13 16:45:46 +0300

    Add test for Profile Async Extra reducers

--- src/entities/Profile/model/slice/profileSlice.test.ts
+++ src/entities/Profile/model/slice/profileSlice.test.ts
@@ -1,4 +1,10 @@
-import { profileActions, profileReducer, ProfileSchema } from 'entities/Profile'
+import {
+  profileActions,
+  profileReducer,
+  ProfileSchema,
+  updateProfileData,
+  ValidateProfileError
+} from 'entities/Profile'
 import { Country } from 'entities/Country'
 import { Currency } from 'entities/Currency'
 
@@ -35,7 +41,7 @@ describe('profileSlice.test', () => {
   })
 
   test('test update full profile', () => {
-    const state: DeepPartial<ProfileSchema> = { form: { username: '' } }
+    const state: DeepPartial<ProfileSchema> = { form: { username: 'here' } }
 
     expect(
       profileReducer(
@@ -44,6 +50,39 @@ describe('profileSlice.test', () => {
       )
     ).toEqual({
       form: { username: 'new' }
+    })
+  })
+
+  test('test update profile service pending', () => {
+    const state: DeepPartial<ProfileSchema> = {
+      isLoading: false,
+      validateErrors: [ValidateProfileError.SERVER_ERROR]
+    }
+
+    expect(
+      profileReducer(state as ProfileSchema, updateProfileData.pending)
+    ).toEqual({
+      isLoading: true,
+      validateErrors: undefined
+    })
+  })
+
+  test('test update profile service fulfilled', () => {
+    const state: DeepPartial<ProfileSchema> = {
+      isLoading: true
+    }
+
+    expect(
+      profileReducer(
+        state as ProfileSchema,
+        updateProfileData.fulfilled(data, '')
+      )
+    ).toEqual({
+      isLoading: false,
+      validateErrors: undefined,
+      readonly: true,
+      data,
+      form: data
     })
   })
 })
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -1,14 +1,15 @@
-import React from "react"
-import { ComponentStory, ComponentMeta } from "@storybook/react"
-import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator"
-import { Theme } from "app/providers/ThemeProvider"
-import { Sidebar } from "./Sidebar"
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator'
+import { Theme } from 'app/providers/ThemeProvider'
+import { Sidebar } from './Sidebar'
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator'
 
 export default {
-  title: "widget/Sidebar",
+  title: 'widget/Sidebar',
   component: Sidebar,
   argTypes: {
-    backgroundColor: { control: "color" }
+    backgroundColor: { control: 'color' }
   }
 } as ComponentMeta<typeof Sidebar>
 
@@ -16,8 +17,15 @@ const Template: ComponentStory<typeof Sidebar> = args => <Sidebar {...args} />
 
 export const Light = Template.bind({})
 Light.args = {}
+Light.decorators = [StoreDecorator({ user: { authData: {} } })]
 
 export const Dark = Template.bind({})
 Dark.args = {}
+Dark.decorators = [
+  ThemeDecorator(Theme.DARK),
+  StoreDecorator({ user: { authData: {} } })
+]
 
-Dark.decorators = [ThemeDecorator(Theme.DARK)]
+export const NoAuth = Template.bind({})
+NoAuth.args = {}
+NoAuth.decorators = [StoreDecorator({ user: {} })]
